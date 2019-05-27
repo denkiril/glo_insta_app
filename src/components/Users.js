229d8802e422501ad111e9d17029b56005bad350
Денис Кирилюк <denkiril@gmail.com>
2019-05-27 17:08:03 +0300

    add class Users (homework)

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,34 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
 import User from './User';
+import InstaService from '../services/instaservice';
+import ErrorMessage from './ErrorMessage';
 
-export default function Users() {
-    return (
-        <div className="right">
-            <User
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="alt"
-                name="name"/>
-            <div className="users__block">
-            <User
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="alt"
-                name="name"
-                min
-                />
-            <User
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="alt"
-                name="name"
-                min
-                />
-            <User
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                alt="alt"
-                name="name"
-                min
-                />
+export default class Users extends Component {
+    InstaService = new InstaService();
+    state = {
+        posts: [],
+        error: false
+    }
+
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts() {
+        this.InstaService.getAllPosts()
+        .then(this.onPostsLoaded)
+        .catch(this.onError);
+    }
+
+    onPostsLoaded = (posts) => {
+        this.setState({
+            posts,
+            error: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map(item => {
+            const {name, altname, photo, id} = item;
+
+            return (
+                <User
+                    src={photo}
+                    alt={altname}
+                    name={name}
+                    min
+                    />
+            )
+        })
+    }
+
+    render() {
+        const {error, posts} = this.state;
+        if (error) {
+            return <ErrorMessage/>
+        }
+
+        const items = this.renderItems(posts);
+
+        return (
+            <div className="right">
+                <User
+                    src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                    alt="alt"
+                    name="noname"
+                    />
+                <div className="users__block">
+                    {items}
+                </div>
             </div>
-        </div>
-
-    )
+        )
+    }
 }
